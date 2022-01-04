c2ac8a53edd1717f21c40d0b81523b6826e3a39c
RoshanKM <roshan.techacc88@gmail.com>
2022-01-04 19:12:16 +0530

    API Gateway implementation

---
+++ src/main/java/com/rkmgithubacc/mba/apigateway/MbaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rkmgithubacc.mba.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbaApiGatewayApplication.class, args);
+	}
+
+}
